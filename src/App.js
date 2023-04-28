c13ee13be05257a8e4f8941299d22d1abfb4da12
KiaFIfe <likiafife1@googlemail.com>
2023-04-28 12:45:21 +0100

    Added the Github repository link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import './App.css';
+import "./App.css";
 
-export default function App(){
-  return 
+export default function App() {
+  return (
+    <div>
+      <h1>React Weather</h1>
+
+
+      <footer>
+        <p>This project was cooded by Likia Fife</p>
+        <a href="https://github.com/KiaFIfe/React-weather-app" target="_blank">
+          Open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
