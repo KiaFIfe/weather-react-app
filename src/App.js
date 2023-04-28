2173f59309c143d395b0381bae11e857b389ee80
KiaFIfe <likiafife1@googlemail.com>
2023-04-28 13:08:46 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 
+
 export default function App() {
   return (
-    <div>
-      <h1>React Weather</h1>
+    <div className="App">
+      <div className="Container">
+        <h1>React Weather</h1>
 
-
-      <footer>
-        <p>This project was cooded by Likia Fife</p>
-        <a href="https://github.com/KiaFIfe/React-weather-app" target="_blank">
-          Open sourced on GitHub
-        </a>
-      </footer>
+        <footer>
+          <p className="m-5">This project was cooded by Likia Fife</p>
+          <a
+            href="https://github.com/KiaFIfe/React-weather-app"
+            target="_blank"
+          >
+            Open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
