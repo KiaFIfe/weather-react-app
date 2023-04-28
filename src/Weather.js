e9524b6e769154a874a7031907bb410b1511a11a
KiaFIfe <likiafife1@googlemail.com>
2023-04-28 20:54:42 +0100

    Replaced some of the fake data using the API

--- src/Weather.js
+++ src/Weather.js
@@ -1,37 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 import Forecast from "./Forecast";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather-container ">
-      <div className="container p-4">
-        {" "}
-        
-        <div className="row">
-          <div className="col">
-            <img src="" alt="weather icon" />
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({run: false});
+  function showWeather(response) {
+    setWeatherData({
+      icon : response.data.weather[0].icon,
+      run: true,
+      temp: response.data.main.temp,
+      humid: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      iconUrl: `https://openweathermap.org/img/wn/${icon}@2x.png`
+    });
+  }
+  function handleSubmit(event) {
+    event.PreventDefault();
+  }
+
+  if (weatherData.run) {
+    return (
+      <div className="Weather-container ">
+        <div className="container p-4">
+          {" "}
+          <h2 className="mb-3">{city}</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search for a city"
+              onChange={(cityName) => setCity(cityName.target.value)}
+            />
+            <input type="submit" />
+          </form>
+          <div className="row">
+            <div className="col">
+              <img src={weatherData.iconUrl} alt="Weather icon"/>
+
+            </div>
+            <div className="col temp">{Math.round(weatherData.temp)}°c</div>
+            <div className="col">
+              <ul>
+                <li>Humidity: {Math.round(weatherData.humid)}</li>
+                <li>Wind: {Math.round(weatherData.wind)} mph</li>
+              </ul>
+            </div>
+            <div className="col">
+              <ul>
+                <li>
+                  <h3>Weather</h3>
+                </li>
+                <li>Friday 13:00</li>
+                <li className="text-capitalize">{weatherData.description}</li>
+              </ul>
+            </div>
           </div>
-          <div className="col temp">13.c</div>
-          <div className="col">
-            <ul>
-              <li>Precipitation: 4%</li>
-              <li>Humidity: 80%</li>
-              <li>Wind: 12 mph</li>
-            </ul>
-          </div>
-          <div className="col">
-            <ul>
-              <li>
-                <h3>Weather</h3>
-              </li>
-              <li>Friday 13:00</li>
-              <li>Cloudy</li>
-            </ul>
-          </div>
+          <Forecast />
         </div>
-       <Forecast/>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Paris";
+    const apiKey = "f49ee1e2561369c11af5b8f8810cf134";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showWeather);
+
+    return "Loading...";
+  }
 }
